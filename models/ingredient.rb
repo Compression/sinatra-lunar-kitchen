b604e958c1e45caccf82c669d190a53542dd2af2
Josh <rkb.josh@gmail.com>
2014-09-08 17:15:49 -0400

    removed unused files, first version to pass all rspec tests

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,8 +1,3 @@
-class Ingredient
-  attr_accessor :id, :name
-
-  def initialize(ingredient_data)
-    @id = ingredient_data['id']
-    @name = ingredient_data['name']
-  end
+class Ingredient < ActiveRecord::Base
+  belongs_to :recipe
 end
